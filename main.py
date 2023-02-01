aaf2f376e1531ff7f99d368ccfe2ffca82c1e8f3
Inter-Aizat <118783079+Inter-Aizat@users.noreply.github.com>
2023-02-01 11:44:26 +0800

    v1

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import os, sys
+
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+elif __file__:
+    application_path = os.path.dirname(__file__)
+
+print(fr'{application_path}\INPUT')
